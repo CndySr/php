b840326670429a89896fba59224092ad34728b06
cindy jesika sari <cndysr42@gmail.com>
2023-01-18 07:50:34 +0700

    kalkulator.php

---
+++ form.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>buku tamu</title>
+</head>
+
+<body>
+    <h1>buku tamu</h1>
+    <hr>
+    <form action="proses.php" method="POST">
+        nama: <input type="text" name='nama'><br>
+        e-mail : <input type='email' name='email'><br>
+        pesan: <textarea name='pesan'></textarea><br><br>
+
+        <input type='submit' name='simpan' value='simpan'>
+
+    </form>
+</body>
+
+</html>
